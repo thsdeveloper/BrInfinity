0f817c7eca262b312835e84f7e2ed8175dad7772
Thiago Soares <ths.pereira@gmail.com>
2017-12-14 12:21:20 -0200

    welcome
    atualizacao de conteudo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravelsss
+                    Brinfinite 1.0
                 </div>
 
                 <div class="links">
