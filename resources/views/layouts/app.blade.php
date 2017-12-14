130553cab9ffe925c49537d3851ab4917779255f
Thiago Soares <ths.pereira@gmail.com>
2017-12-14 12:18:58 -0200

    Header
    Alteracao na header para position sluid

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,7 +20,7 @@
     <?php $checkAdmin = Voyager::can('browse_admin'); ?>
     <div id="app">
         <nav class="navbar navbar-default navbar-static-top">
-            <div class="container">
+            <div class="container-fluid">
                 <div class="navbar-header">
 
                     <!-- Collapsed Hamburger -->
@@ -50,23 +50,33 @@
                             <li><a href="{{ route('login') }}">Login</a></li>
                             <li><a href="{{ route('register') }}">Register</a></li>
                         @else
+                          <li><a href="{{ url('cotacao') }}">Cotação</a></li>
+
+                          <li class="dropdown">
+                              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true">
+                                  Produção <span class="caret"></span>
+                              </a>
+
+                              <ul class="dropdown-menu">
+                                  @if($checkAdmin)
+                                    <li><a href="{{ url('producao') }}">Geral</a></li>
+                                    <li><a href="{{ url('relatorio') }}">Relatório Anual</a></li>
+                                  @endif
+                              </ul>
+                          </li>
+
+
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true">
                                     {{ Auth::user()->name }} <span class="caret"></span>
                                 </a>
 
                                 <ul class="dropdown-menu">
-                                    @if($checkAdmin)
-                                      <li><a href="{{ url('producao') }}">Produção Geral</a></li>
-                                    @endif
-                                    <li><a href="{{ url('cotacao') }}">Cotação</a></li>
-                                    <li><a href="{{ url('producao') }}/{{ Auth::user()->id }}">Produção</a></li>
-
                                     <li>
                                         <a href="{{ route('logout') }}"
                                             onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
-                                            Logout
+                                            Sair do sistema
                                         </a>
 
                                         <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
